7c20c988d903fdc0cba3fd8f1da0a525e1b160fc
ryuimoto <narrow617@gmail.com>
2021-01-19 12:52:54 +0900

    transition add

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:docokana_app/routes/menu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Menu(),
     );
   }
 }
---
+++ lib/routes/menu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FirstPage'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () => {},
+                child: Text('Nextページへ'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
