76a096b6d4d79be9a5d1e5951853bbaafe0b941d
ryuimoto <narrow617@gmail.com>
2021-01-19 18:30:57 +0900

    font add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:docokana_app/routes/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
--- lib/routes/menu.dart
+++ lib/routes/menu.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('メインメニュー'),
+        title: Text(
+          'MainMenu',
+          style: GoogleFonts.getFont('GFS Didot'),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.lightGreen,
       ),
       body: Container(
         padding: EdgeInsets.all(32.0),
         child: Center(
           child: Column(
             children: <Widget>[
+              Text(
+                'Who are you?',
+                style: GoogleFonts.getFont('GFS Didot'),
+              ),
+              SizedBox(height: 100),
               RaisedButton(
                 onPressed: () => {},
                 child: Text('依頼者'),
               ),
               RaisedButton(
-                onPressed: () => {},
-                child: Text('情報提供者'),
-              )
+                child: const Text('情報提供者'),
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {},
+              ),
             ],
           ),
         ),
