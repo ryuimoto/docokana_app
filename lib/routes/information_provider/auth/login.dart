258ede7a8a1d4a17f7cb72a8dca2356908ec09e7
ryuimoto <narrow617@gmail.com>
2021-01-20 16:33:12 +0900

    controller folder add

---
+++ lib/routes/information_provider/auth/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FirstPage'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
